bb64fe4be122147106a2d0d5273435dd46813709
4blacktop <4blogga@gmail.com>
2016-01-21 17:02:14 +1000

    1702

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -24,50 +24,32 @@ var mainView = myApp.addView('.view-main', {
     // domCache: true// Enable Dom Cache so we can use all inline pages
 });
 
-// alert code
+
+// alert code 1st home
+console.log( "1st home init" );
 $$('.alert-text-title').on('click', function () {
-    myApp.alert(mainView.activePage.name, 'Custom Title!');
+    myApp.alert(mainView.activePage.name, 'Home!');
 });
 
-// Click event on link to Category List Page
+// Click event 1st home on link to Category List Page
 $$('.load-dynamic-page-category').on('click', function(){
 	var catid = $$(this).attr('data-catid');
 	var categoryUrl = 'http://27biletov.ru/wp-json/wp/v2/posts?filter[cat]='+catid;
-	// console.log( "catid:" + catid + "; carURL: " + categoryUrl);
-	
+	console.log( "catid:" + catid + "; carURL: " + categoryUrl);
 	$$.getJSON(categoryUrl, function (json) {
-	// console.log( json );
+	console.log( json );
 	var pageContent = myApp.categoryTemplate(json);
 	// console.log( pageContent );
 	mainView.loadContent(pageContent);
 	});
 });
-/* 
+
 // Click event on link to Post Page
 $$('.load-dynamic-page-post').on('click', function(){
-	alert( postid );
 	var postid = $$(this).attr('data-postid');
-	console.log( postid );
-	var postUrl = 'http://27biletov.ru/wp-json/wp/v2/posts/'+postid;
-	console.log( "postid:" + postid + "; postURL: " + postUrl);
-	
-	// $$.getJSON(postUrl, function (json) {
-	// console.log( json );
-	// var pageContent = myApp.postTemplate(json);
-	// console.log( pageContent );
-	// mainView.loadContent(pageContent);
-	// });
-});
- */
- 
-// Build details page
-$$('.load-dynamic-page-post').on('click', 'a.item-link', function (e) {
-	var postid = $$(this).attr('data-postid');
-	var postUrl = 'http://27biletov.ru/wp-json/wp/v2/posts/'+postid;
 	// console.log( postid );
+	var postUrl = 'http://27biletov.ru/wp-json/wp/v2/posts/'+postid;
 	console.log( "postid:" + postid + "; postURL: " + postUrl);
-	
-	
 	
 	$$.getJSON(postUrl, function (json) {
 	console.log( json );
@@ -75,6 +57,149 @@ $$('.load-dynamic-page-post').on('click', 'a.item-link', function (e) {
 	// console.log( pageContent );
 	mainView.loadContent(pageContent);
 	});
+});
+
+	// debug-info
+	$$('.debug-info').on('click', function(){
+		// console.log( page);
+		console.log( mainView.activePage.name);
+		
+		// console.log( mainView.params);
+		// console.log( mainView.history);
+		// console.log( mainView.contentCache);
+		// console.log( mainView.url);
+		// console.log( mainView.pagesContainer);
+		// console.log( mainView.activePage);
+		// console.log( mainView.main);
+		// console.log( mainView.router);
+		});
+
+
+
+// Initializing Home Page
+myApp.onPageInit('home',function(page){
+	// alert
+	console.log( "category init" );
+	$$(page.container).on('click','.alert-text-title',function(){
+		console.log( "function alert" );
+		myApp.alert(mainView.activePage.name, 'Home!');
+	});
+	
+	// debug-info
+	$$('.debug-info').on('click', function(){
+		// console.log( page);
+		console.log( mainView.activePage.name);
+		
+		// console.log( mainView.params);
+		// console.log( mainView.history);
+		// console.log( mainView.contentCache);
+		// console.log( mainView.url);
+		// console.log( mainView.pagesContainer);
+		// console.log( mainView.activePage);
+		// console.log( mainView.main);
+		// console.log( mainView.router);
+		});
+	
+	// Click event 1st home on link to Category List Page
+	$$('.load-dynamic-page-category').on('click', function(){
+		var catid = $$(this).attr('data-catid');
+		var categoryUrl = 'http://27biletov.ru/wp-json/wp/v2/posts?filter[cat]='+catid;
+		console.log( "catid:" + catid + "; carURL: " + categoryUrl);
+		$$.getJSON(categoryUrl, function (json) {
+		console.log( json );
+		var pageContent = myApp.categoryTemplate(json);
+		// console.log( pageContent );
+		mainView.loadContent(pageContent);
+		});
+	
+	
+	// Click event on link to Post Page
+	$$('.load-dynamic-page-post').on('click', function(){
+		var postid = $$(this).attr('data-postid');
+		// console.log( postid );
+		var postUrl = 'http://27biletov.ru/wp-json/wp/v2/posts/'+postid;
+		console.log( "postid:" + postid + "; postURL: " + postUrl);
+		
+		$$.getJSON(postUrl, function (json) {
+		console.log( json );
+		var pageContent = myApp.postTemplate(json);
+		// console.log( pageContent );
+		mainView.loadContent(pageContent);
+		
+		
+		});
+	});	
+});
+
+
+});
+
+
+
+// Initializing Category Page
+myApp.onPageInit('category',function(page){
+	// alert
+	console.log( "category init" );
+	$$(page.container).on('click','.alert-text-title',function(){
+		console.log( "function alert" );
+		myApp.alert(mainView.activePage.name, 'Category!');
+	});
+	
+	// debug-info
+	$$('.debug-info').on('click', function(){
+		// console.log( page);
+		console.log( mainView.activePage.name);
+		
+		// console.log( mainView.params);
+		// console.log( mainView.history);
+		// console.log( mainView.contentCache);
+		// console.log( mainView.url);
+		// console.log( mainView.pagesContainer);
+		// console.log( mainView.activePage);
+		// console.log( mainView.main);
+		// console.log( mainView.router);
+		});
+	
+	// Click event on link to Post Page
+	$$('.load-dynamic-page-post').on('click', function(){
+		var postid = $$(this).attr('data-postid');
+		// console.log( postid );
+		var postUrl = 'http://27biletov.ru/wp-json/wp/v2/posts/'+postid;
+		console.log( "postid:" + postid + "; postURL: " + postUrl);
+		
+		$$.getJSON(postUrl, function (json) {
+		console.log( json );
+		var pageContent = myApp.postTemplate(json);
+		// console.log( pageContent );
+		mainView.loadContent(pageContent);
+		});
+	});
+});
+
+
+// Initializing Post Page
+myApp.onPageInit('post',function(page){
+	// alert
+	console.log( "post init" );
+	$$(page.container).on('click','.alert-text-title',function(){
+		console.log( "function alertfunction alertfunction alertfunction alert" );
+		myApp.alert(mainView.activePage.name, 'Post!');
+	});
+	
+	// debug-info
+	$$('.debug-info').on('click', function(){
+		// console.log( page);
+		console.log( mainView.activePage.name);
+		
+		// console.log( mainView.params);
+		// console.log( mainView.history);
+		// console.log( mainView.contentCache);
+		// console.log( mainView.url);
+		// console.log( mainView.pagesContainer);
+		// console.log( mainView.activePage);
+		// console.log( mainView.main);
+		// console.log( mainView.router);
+		});
 });
 
 
